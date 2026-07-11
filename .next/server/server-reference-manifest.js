f7eede2d56b53c322ba9d92ec0b6500c0a6e65ae
Shohag <contact.devshohag@gmail.com>
2026-07-11 20:36:26 +0600

    chore(frontend): initialize Next.js workspace with core dependencies and routing structure

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qLcV7A8FEZPLAfFofFpXLjgziwArVbjwrI3Kyh8MDsE=\"\n}"
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+// API and external services
